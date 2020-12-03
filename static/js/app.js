dcaacbcaaef5bab42fe551f6f3b6db928aca5899
rmotta1992 <68138899+rmotta1992@users.noreply.github.com>
2020-12-02 19:07:44 -0500

    addding lines for dropdown menu to change graph

--- static/js/app.js
+++ static/js/app.js
@@ -15,15 +15,31 @@ d3.json("../data/samples.json").then(function(data) {
   //   var barChart = document.getElementById('bar');
   //   barChart.innerHTML = 'Selected Bird: <b>' + ele.options[ele.selectedIndex].text + '</b> </br>' +
   //       'ID: <b>' + ele.value + '</b>';
+  function handleSubmit() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    // Select the input value from the form
+    var stock = d3.select("#selDataset").node().value;
+    console.log(stock);
+  
+    // clear the input value
+    d3.select("#selDataset").node().value = "";
+  
+    // Build the plot with the new stock
+    buildPlot(stock);
+  }
+  function buildPlot(stock) {
+    var trace1 = {
+      x: sampleValues,
+      y: otuIds,
+      type : "bar",
+      orientation: "h"
+    };
+    var barData = [trace1]
 
-var trace1 = {
-  x: [data.samples.sample_values],
-  y: [data.samples.otu_ids],
-  type : "bar",
-  orientation: "h"
-};
-var barData = [trace1]
+    Plotly.newPlot("bar", barData);
 
-Plotly.newPlot("bar", barData);
-
+    };
 });
+
+//d3.select("#selDataset").on("click", handleSubmit);
